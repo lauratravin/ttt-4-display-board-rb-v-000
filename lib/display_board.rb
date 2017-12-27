90ffcb96c8bc1d718c4e18bcd7358afb84ad8f0d
Laura M. Travin <laura.travin@gmail.com>
2017-12-27 20:42:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-game
+
 def display_board(game)
   puts ' #{game[0]} | #{game[1]} | #{game[2]} '
   puts '-----------'
