30b6166fafa607e6d1422a7079fe0db99b71ba87
Laura M. Travin <laura.travin@gmail.com>
2017-12-27 20:39:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ game
 def display_board(game)
   puts ' #{game[0]} | #{game[1]} | #{game[2]} '
   puts '-----------'
-  puts '   |   |   '
+  puts ' #{game[3]} | #{game[4]} | #{game[5]} '
   puts '-----------'
-  puts '   |   |   '
+  puts ' #{game[6]} | #{game[7]} | #{game[8]} '
 end
